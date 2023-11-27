b13c5ce141cdf998df7cd5d5cd819465caf7aaa4
MarkulakS <markulaks17@gmail.com>
2023-11-27 21:36:36 +0100

    Making hamburger menu

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -12,4 +12,19 @@ export class HomePageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  departmenst = [
+    { label: 'Wydział Chemii', link: '#' },
+    { label: 'Wydział Ekonomiczny', link: '#' },
+    { label: 'Wydział Filologiczny', link: '#' },
+    { label: 'Wydział Lekarski', link: '#' },
+    { label: 'Wydział Matematyki, Fizyki i Informatyki', link: '#' },
+    { label: 'Wydział Nauk o Polityce i Komunikacji Społecznej', link: '#' },
+    { label: 'Wydział Nauk o Zdrowiu', link: '#' },
+    { label: 'Wydział Nauk Społecznych', link: '#' },
+    { label: 'Wydział Prawa i Administracji', link: '#' },
+    { label: 'Wydział Przyrodniczo-Techniczny', link: '#' },
+    { label: 'Wydział Sztuki', link: '#' },
+    { label: 'Wydział Teologiczny', link: '#' }
+  ]
+
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -15,6 +15,7 @@ export class NavbarComponent implements OnInit {
   mainLinks = [
     { 
       label: 'uczelnia',
+      id: 'flush-collapseOne',
       links: [
         {label: 'wybory', url: '#'},
         {label: 'kontakt', url: '#'},
@@ -27,6 +28,7 @@ export class NavbarComponent implements OnInit {
     },
     { 
       label: 'kandydat',
+      id: 'flush-collapseTwo',
       links: [
         {label: 'studia 2023/2024', url: '#'},
         {label: 'szkoła doktorska', url: '#'},
@@ -37,62 +39,68 @@ export class NavbarComponent implements OnInit {
     },
     { 
       label: 'student',
+      id: 'flush-collapseThree',
       links: [
         {label: 'wybory', url: '#'},
         {label: 'kontakt', url: '#'},
-        {label: 'o uczelni', url: '#'},
-        {label: 'strukura', url: '#'},
-        {label: 'władze', url: '#'},
-        {label: 'monitor uo', url: '#'},
-        {label: 'strategia rozwoju', url: '#'}
+        {label: 'E-learning - platforma Moodle', url: '#'},
+        {label: 'Kalendarz akademicki', url: '#'},
+        {label: 'Samorząd Studencki', url: '#'},
+        {label: 'USOS', url: '#'},
+        {label: 'Kampus UO', url: '#'},
+        {label: 'Erasmus', url: '#'},
+        {label: 'Sport', url: '#'},
+        {label: 'Legia Akademicka', url: '#'},
+        {label: 'Koła Naukowe', url: '#'},
+        {label: 'Zaproś wykładowcę', url: '#'},
+        {label: 'Akademickie Centrum Karier', url: '#'}
       ]
     },
     { 
-      label: 'doktorant',
-      links: [
-        {label: 'wybory', url: '#'},
-        {label: 'kontakt', url: '#'},
-        {label: 'o uczelni', url: '#'},
-        {label: 'strukura', url: '#'},
-        {label: 'władze', url: '#'},
-        {label: 'monitor uo', url: '#'},
-        {label: 'strategia rozwoju', url: '#'}
-      ]
+      label: 'doktorant'
     },
     { 
       label: 'pracownik',
+      id: 'flush-collapseFive',
       links: [
-        {label: 'wybory', url: '#'},
-        {label: 'kontakt', url: '#'},
-        {label: 'o uczelni', url: '#'},
-        {label: 'strukura', url: '#'},
-        {label: 'władze', url: '#'},
-        {label: 'monitor uo', url: '#'},
-        {label: 'strategia rozwoju', url: '#'}
+        {label: 'Aktualności pracownicze', url: '#'},
+        {label: 'Pracownicze Plany Kapitałowe', url: '#'},
+        {label: 'Oferta grantów', url: '#'},
+        {label: 'E-learning - platforma Moodle', url: '#'},
+        {label: 'Oferty pracy', url: '#'},
+        {label: 'Biuro Spraw Pracowniczych', url: '#'},
+        {label: 'Elektroniczna legitymacja służbowa', url: '#'},
+        {label: 'Dokumenty do pobrania', url: '#'},
+        {label: 'Związki zawodowe', url: '#'},
+        {label: 'SIP', url: '#'},
+        {label: 'Szukaj', url: '#'}
       ]
     },
     { 
       label: 'nauka',
+      id: 'flush-collapseSix',
       links: [
-        {label: 'wybory', url: '#'},
-        {label: 'kontakt', url: '#'},
-        {label: 'o uczelni', url: '#'},
-        {label: 'strukura', url: '#'},
-        {label: 'władze', url: '#'},
-        {label: 'monitor uo', url: '#'},
-        {label: 'strategia rozwoju', url: '#'}
+        {label: 'Postępowania awansowe', url: '#'},
+        {label: 'Jednostki badawcze', url: '#'},
+        {label: 'Granty', url: '#'},
+        {label: 'Czasopisma ', url: '#'},
+        {label: 'Wydawnictwa UO', url: '#'},
+        {label: 'Baza Wiedzy UO', url: '#'},
+        {label: 'Współpraca z zagranicą', url: '#'},
+        {label: 'Projekty finansowane z budżetu państwa', url: '#'},
       ]
     },
     { 
       label: 'biznes',
+      id: 'flush-collapseSeven',
       links: [
-        {label: 'wybory', url: '#'},
-        {label: 'kontakt', url: '#'},
-        {label: 'o uczelni', url: '#'},
-        {label: 'strukura', url: '#'},
-        {label: 'władze', url: '#'},
-        {label: 'monitor uo', url: '#'},
-        {label: 'strategia rozwoju', url: '#'}
+        {label: 'Oferta dla biznesu', url: '#'},
+        {label: 'Inkubator Przedsiębiorczości', url: '#'},
+        {label: 'Uniwersyteckie Centrum Transferu Wiedzy i Technologii', url: '#'},
+        {label: 'Umowy i porozumienia', url: '#'},
+        {label: 'Wynajem sal', url: '#'},
+        {label: 'Potencjał intelektualny', url: '#'},
+        {label: 'Partnerzy', url: '#'}
       ]
     }
   ]
