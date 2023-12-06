788c2b36d0cb779bf1c026a08398d5bfd5e1f2d3
MarkulakS <markulaks17@gmail.com>
2023-12-06 19:14:16 +0100

    Macro changes of responsive carousel

--- src/app/components/usages/gallery/gallery.component.ts
+++ src/app/components/usages/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -7,13 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
   }
 
-  showGallery() {
-    console.log('show more gallery');
+  ngOnInit(): void {
   }
 
   row1 = [
